61b1683097df3a328a395461e9444389ec5a874e
ntelegey <71426012+ntelegey@users.noreply.github.com>
2021-03-12 09:48:00 +0600

    Update index.php

--- index.php
+++ index.php
@@ -6,15 +6,14 @@
 <body style="background-color: #ffe6cc;">
 <h1 style="font-size:42px;text-align:center; color:#14106b;">Personal Information</h1>
 <p> <img src="smt.jpg" alt="Photo" style="float:left;width:282px;height:276px;" >
- Name: Telegey Nurbekova<br>
-<br>
- Date of Bitrh: 02.01.2004<br><br>
- ID Number: T180300004 <br><br>
- Level: College<br><br>
- Faculty: Secondary Vocational School<br><br>
- Department: Software of computer facilities and automated systems<br><br>
- Email: telegey.nurbekova@iaau.edu.kg<br><br>
- Education language: English
+  Name: Telegey Nurbekova<br><br>
+  Date of Bitrh: 02.01.2004<br><br>
+  ID Number: T180300004 <br><br>
+  Level: College<br><br>
+  Faculty: Secondary Vocational School<br><br>
+  Department: Software of computer facilities and automated systems<br><br>
+  Email: telegey.nurbekova@iaau.edu.kg<br><br>
+  Education language: English
 </p>
 </body>
 </html>
