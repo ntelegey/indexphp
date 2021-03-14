b67746f56649e670c3ae13ba3e7723cc0de7d988
ntelegey <71426012+ntelegey@users.noreply.github.com>
2021-03-14 18:37:33 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> PornHub </title>
+	<title> Telegey Nurbekova </title>
 </head>
-<body style="background-color:pink;">
+<body style="background-color:beige;">
 <h1 style="font-size:42px;text-align:center; color:#14106b;">Personal Information</h1>
 <p> <img src="smt.jpg" alt="Photo" style="float:left;width:282px;height:276px;" >
-  Porn model: Telegey Nurbekova<br><br>
+  Name: Telegey Nurbekova<br><br>
   Date of Bitrh: 02.01.2004<br><br>
   ID Number: T180300004 <br><br>
   Level: College<br><br>
-  Faculty: Sex<br><br>
+  Faculty: Vocational school<br><br>
   Department: Software of computer facilities and automated systems<br><br>
   Email: telegey.nurbekova@iaau.edu.kg<br><br>
   Education language: English
