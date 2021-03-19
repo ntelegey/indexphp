58935f8289f716909218252d5728afcb1831051b
ntelegey <71426012+ntelegey@users.noreply.github.com>
2021-03-19 10:33:35 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <html>
 <body>
 
-<form action="main.php" method="get">
-Login: <input type="text" name="login"><br><br>
-Password: <input type="text" name="password"><br><br>
-<button type="Submit" value="Submit">Submit</button>
-<button type="Reser" value="Reset">Clear</button>
+
+<div class="container">
+    <label for="login"><b>Login</b></label>
+    <input type="text" placeholder="Enter Username" name="Telegey" required>
+
+    <label for="Password"><b>Password</b></label>
+    <input type="Password" placeholder="Enter Password" name="1234" required>
+    <br>
+
+    <button type="submit">Login</button>
+    <br>
+   
+ </div>
+
+  <div class="container" style="background-color:#ffe6cc">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+
 </form>
+
+
 
 </body>
 </html>
