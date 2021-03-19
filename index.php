195f9d89d155ef307f4687a891590768e7f9158d
ntelegey <71426012+ntelegey@users.noreply.github.com>
2021-03-19 09:13:11 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
-<!DOCTYPE html>
 <html>
-<head>
-	<title> Telegey Nurbekova </title>
-</head>
-<body style="background-color:beige;">
-<h1 style="font-size:42px;text-align:center; color:#14106b;">Personal Information</h1>
-<p> <img src="smt.jpg" alt="Photo" style="float:left;width:282px;height:276px;" >
-  Name: Telegey Nurbekova<br><br>
-  Date of Bitrh: 02.01.2004<br><br>
-  ID Number: T180300004 <br><br>
-  Level: College<br><br>
-  Faculty: Vocational school<br><br>
-  Department: Software of computer facilities and automated systems<br><br>
-  Email: telegey.nurbekova@iaau.edu.kg<br><br>
-  Education language: English
-</p>
+<body>
+
+<form action="index.php" method="get">
+Login: <input type="text" name="login"><br>
+Password: <input type="text" name="password"><br>
+<input type="submit">
+</form>
+
+<php
+
+$login = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $login = test_input($_POST["login"]);
+  $password = test_input($_POST["password"]);
+
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+?>
+
 </body>
 </html>
