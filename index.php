0b6df7d96e33abf3fa20e487b958728e840f7ba2
ntelegey <71426012+ntelegey@users.noreply.github.com>
2021-03-19 09:32:03 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<form action="index.php" method="get">
+Login: <input type="text" name="login"><br><br>
+Password: <input type="text" name="password"><br><br>
+<button type="Submit" value="Submit">Submit</button>
+<button type="Reser" value="Reset">Clear</button>
+</form>
+
+</body>
+</html>
