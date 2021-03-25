07f6b032a5d80fd1a1942475d811d24a19701db2
ntelegey <71426012+ntelegey@users.noreply.github.com>
2021-03-25 23:09:59 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Forms</title>
+<title>Forms</title>
 </head>
 <body style="background-color: #ffe6cc;">
 <h1 style="font-size:36px;text-align:center; color:#14106b;">Sign in</h1>
@@ -9,18 +9,17 @@
 	<div class="container" align="center">
 		<br>
 		<label  align="center" for="username">Username</label>
-		<input  align="center" type="text" placeholder="Input Username" class="fname" id="fname" name="fname"><br>
-		<div class="pass_wrap">
-		<label  align="center" for="password" class="pass_title">Password</label>
-		<input  align="center" type="password" placeholder="Input Password" class="fpass" id="fpass" name="password"><br>
-	    </div>
+		<input type="text" placeholder="Input Username"  id="fname"><br>
+		<label  align="center" for="password">Password</label>
+		<input type="password" placeholder="Input Password" id="fpass" <br>
+	      
 	    <div class="form_button">
-	<br>
-	<button  align="center" type="submit" align="center" class="submit_button" id="submit_button" value="button">Submit</button>
-	<button  align="center" type="reset" align="center" class="clear_button" value="clear">Clear</button>
-				<br><br>
-	</div>	
-</div>
+	    <br>
+	    <button type="submit" align="center" value="submit">Submit</button>
+	    <button type="reset" align="center" value="clear">Clear</button> <br><br>
+	    </div>
+		
+	</div>
 </form>
 
 	
@@ -31,7 +30,7 @@
         let login = document.getElementById('fname').value;
         let password = document.getElementById('fpass').value;
  
-        if ( login === "Telegey" && password === "1234") 
+        if ( login == "Telegey" && password == "1234") 
          return true;
         else {
             e.preventDefault();
@@ -50,7 +49,7 @@
    margin: auto;
    }
 
-  </style>
+</style>
 
 </body>
 </html>
